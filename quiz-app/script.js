dc65644754ff488e59a9eda45a3e08823d2cada4
arshalsoren <arshalsoren@gmail.com>
2021-06-10 20:58:23 +0530

    new project added

---
+++ quiz-app/script.js
@@ -0,0 +1,42 @@
+const quizData = [
+    (
+        question: ' PM Modi has inaugurated E100 ethanol dispensing stations in _______?',
+        a: 'Pune',
+        b: 'New Delhi',
+        c: 'Kolkata',
+        d: 'Mumbai',
+        correct: 'a'
+    ),
+    (
+        question: 'Which firm has launched India’s 1st NFT Marketplace?',
+        a: 'Angel Broking',
+        b: 'WazirX',
+        c: 'Indiabulls',
+        d: 'GrowMore',
+        correct: 'b'
+    ),
+    (
+        question: 'Which State has the cheapest Vegetarian Thali as per the Economic Survey 2019-20?',
+        a: 'Punjab',
+        b: 'Jharkhand',
+        c: 'Uttar Pradesh',
+        d: 'Bihar',
+        correct: 'b'
+    ),
+    (
+        question: 'The Subject Expert Committee (SEC) has approved which COVID-19 vaccine for phase 2 and 3 human clinical trials on 2 to 18-year-olds?',
+        a: 'Covaxin',
+        b: 'Covishield',
+        c: 'J&J',
+        d: 'Sputnik',
+        correct: 'a'
+    ),
+    (
+        question: 'Which vaccine maker has pledged $2,50,000 as humanitarian aid to India?',
+        a: 'AstraZeneca',
+        b: 'Pfizer',
+        c: 'Moderna',
+        d: 'RDIF',
+        correct: 'a'
+    )
+]
