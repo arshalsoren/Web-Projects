306b9834e5af77bafb573833b0ade53bcbb4649e
arshalsoren <arshalsoren@gmail.com>
2021-06-11 22:17:24 +0530

    add - storing data of notes(array) in localStorage

--- notes-app/script.js
+++ notes-app/script.js
@@ -1,10 +1,18 @@
 const addBtn = document.getElementById('add');
 
+const texts=JSON.parse(localStorage.getItem('texts'));
+
+if(texts){
+    texts.forEach(text=>{
+        addNewNote(text);
+    });
+}
+
 addBtn.addEventListener('click', () => {
     addNewNote();
 });
 
-function addNewNote() {
+function addNewNote(text='') {
     const note = document.createElement('div');
     note.classList.add('note');
 
@@ -25,6 +33,8 @@ function addNewNote() {
     const main = note.querySelector('.main');
     const textArea = note.querySelector('textarea');
 
+    textArea.value=text;
+
     editBtn.addEventListener('click', () => {
         main.classList.toggle('hidden');
         textArea.classList.toggle('hidden');
@@ -37,7 +47,21 @@ function addNewNote() {
     textArea.addEventListener('input', (e) => {
         const { value } = e.target;
         main.innerHTML = marked(value);
+
+        updateLocalStorage();
     })
 
     document.body.appendChild(note);
+}
+
+function updateLocalStorage(){
+    const storeText=document.querySelectorAll('textarea');
+
+    const texts=[];
+
+    storeText.forEach(text =>{
+        texts.push(text.value);
+    });
+
+    localStorage.setItem('texts', JSON.stringify(texts));
 }
