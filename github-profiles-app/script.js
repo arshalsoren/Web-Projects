67752678405a61dbafc53c366f8100069ed2c241
arshalsoren <arshalsoren@gmail.com>
2021-06-15 22:08:13 +0530

    add - repos to user's card

--- github-profiles-app/script.js
+++ github-profiles-app/script.js
@@ -1,17 +1,27 @@
-const API_URL = 'https://api.github.com/users/'
+const API_URL = "https://api.github.com/users/"
 
-const main = document.getElementById('main');
-const form = document.getElementById('form');
-const search = document.getElementById('search');
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
-getUserInfo('arshalsoren');
+getUserInfo("arshalsoren");
 
-async function getUserInfo(user) {
-    const response = await fetch(API_URL + user);
+async function getUserInfo(username) {
+    const response = await fetch(API_URL + username);
     const responseData = await response.json();
 
     newUserCard(responseData);
+
+    getReposInfo(username);
 }
+
+async function getReposInfo(username) {
+    const response = await fetch(API_URL + username + "/repos");
+    const responseData = await response.json();
+
+    addToCard(responseData);
+}
+
 
 function newUserCard(user) {
     const cardData = `
@@ -26,13 +36,34 @@ function newUserCard(user) {
                 <ul class="info">
                     <li>${user.followers}<strong>Followers</strong></li>
                     <li>${user.following}<strong>Following</strong></li>
-                    <li>${user.public_repos}<strong>Public Repos</strong></li>
+                    <li>${user.public_repos}<strong>Repositories</strong></li>
                 </ul>
+
+                <h4>Public Repositories</h4>
+                <div id="repos"></div>
             </div>
         </div>
     `;
 
     main.innerHTML = cardData;
+}
+
+function addToCard(repos) {
+    const addRepo = document.getElementById("repos");
+    // console.log(repos);
+
+    repos
+        .sort((a, b) => b.stargazers_count - a.stargazers_count)
+        .forEach((repo) => {
+            const newRepo = document.createElement("a");
+            newRepo.classList.add("repo");
+
+            newRepo.href = repo.html_url;
+            newRepo.target = "_blank";
+            newRepo.innerText = repo.name;
+
+            addRepo.appendChild(newRepo);
+        });
 }
 
 form.addEventListener("submit", (e) => {
